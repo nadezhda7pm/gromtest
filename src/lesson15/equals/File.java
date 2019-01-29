e0c56249a83b85fd85c68f748a60946da0b1a930
cdadmin <nadezhda7pm@gmail.com>
2019-01-29 22:33:07 +0200

    5th commit

---
+++ src/lesson15/equals/File.java
@@ -0,0 +1,45 @@
+package lesson15.equals;
+
+import java.util.Objects;
+
+public class File {
+    private int size;
+    private String path;
+    private String extention;
+
+    public File(int size, String path, String extention) {
+        this.size = size;
+        this.path = path;
+        this.extention = extention;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        File file = (File) o;
+        return Objects.equals(path, file.path);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(path);
+    }
+
+    //    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        File file = (File) o;
+//        return size == file.size &&
+//                Objects.equals(path, file.path) &&
+//                Objects.equals(extention, file.extention);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//
+//        return Objects.hash(size, path, extention);
+//    }
+}
