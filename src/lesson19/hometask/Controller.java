5fae37015334fb90c7debcdf547664f04a205d3a
cdadmin <nadezhda7pm@gmail.com>
2019-02-21 16:16:15 +0200

    16th commit, unit 19 home task

--- src/lesson19/hometask/Controller.java
+++ src/lesson19/hometask/Controller.java
@@ -1,60 +1,30 @@
 package lesson19.hometask;
 
+import static lesson19.hometask.Validator.*;
+
 public class Controller {
 
-
     public static void put(Storage storage, File file) throws Exception {
+
         // if storage or file is null
-        if (storage == null || file == null)
-            try {
-                throw new NullPointerException();
-            } catch (NullPointerException e) {
-                if (storage == null) System.err.println("Storage is null");
-                if (file == null) System.err.println("File is null");
-                return;
-            }
+        validateFileForNULL(file);
+        validateStorageForNULL(storage);
 
         // if file format is wrong
-        int c = 0;
-        for (String format : storage.getFormatsSupported()) {
-            if (file.getFormat().equals(format))
-                c++;
-        }
-        if (c < 1) {
-            try {
-                throw new Exception();
-            } catch (Exception e) {
-                System.err.println("File " + file.getId() + " format is not acceptable");
-                return;
-            }
-        }
+        validateFileFormat(storage, file);
 
         // if storage size is not enough for the putting file
-        if (getStorageActualSize(storage) + file.getSize() > storage.getStorageSize()) try {
-            throw new Exception();
-        } catch (Exception e) {
-            System.err.println("Storage " + storage.getId() + " size is not enough for the putting file " + file.getId());
-            return;
-        }
-
+        validateStorageSizeForFile(storage, file);
 
         // if file exists in the storage
-        try {
-            if (filePresent(storage, file)) {
-                throw new Exception();
-            }
-        } catch (Exception e) {
-            System.err.println("File with id " + file.getId() + " already exists in the storage");
-            return;
-        }
+        validateFilePresent (storage, file);
+
         for (int i = 0; i < storage.getFiles().length; i++) {
             if (storage.getFiles()[i] == null) {
                 storage.getFiles()[i] = file;
                 return;
             }
         }
-
-
     }
 
     public static void delete(Storage storage, File file) {
@@ -66,106 +36,43 @@ public class Controller {
         }
     }
 
-    public static void transferAll(Storage storageFrom, Storage storageTo) {
+    public static void transferAll(Storage storageFrom, Storage storageTo) throws Exception {
         // if storageFrom or storageTo is null
-        try {
-        if (storageFrom == null || storageTo == null)
-                throw new NullPointerException();
-            } catch (NullPointerException e) {
-                if (storageFrom == null) System.err.println("StorageFrom is null");
-                if (storageTo == null) System.err.println("StorageTo is null");
-                return;
-            }
-
+        validateStorageForNULL(storageFrom);
+        validateStorageForNULL(storageTo);
 
         //StorageTo size is not enough for transfer
-        try {
-            if (getStorageActualSize(storageFrom) > storageTo.getStorageSize())
-                throw new Exception();
-        } catch (Exception e) {
-            System.err.println("Storage " + storageTo.getId() + " size is not enough for transfer");
-            return;
-        }
+        validateStorageFromToSize(storageFrom, storageTo);
 
         //FilesArray length in storageTo is not enough for transfer
-        try {
-            if (storageFrom.getFiles().length > storageTo.getFiles().length)
-                throw new ArrayIndexOutOfBoundsException();
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.err.println(e.getMessage());
-            return;
-        }
+        validateStorageToFilesArrayLength(storageFrom, storageTo);
 
         for (int i = 0; i < storageFrom.getFiles().length; i++) {
             try {
                 put(storageTo, storageFrom.getFiles()[i]);
             } catch (Exception e) {
-                System.err.println("File with id " + storageFrom.getFiles()[i].getId() + "was not transferred to " + storageTo.getId());
+                System.err.println("File with id " + storageFrom.getFiles()[i].getId() + "was not transferred to " + storageTo.getId()  + " Reason: " + e.getMessage());
             }
         }
 
     }
 
-    public static void transferFile(Storage storageFrom, Storage storageTo, long id) {
+    public static void transferFile(Storage storageFrom, Storage storageTo, long id) throws Exception {
 
         // if there is no file with such id to transfer
-        try {
-            if (getFileById(storageFrom, id) == null) {
-                throw new Exception();
-            }
-        } catch (Exception e) {
-            System.err.println("File with id " + id + " not found in storage " + storageFrom.getId());
-            return;
-        }
+        validateFilePresentInStorageFrom(storageFrom, id);
 
         //StorageTo actual size is not enough for transfer
-        try {
-            if ((getStorageActualSize(storageTo) + getFileById(storageFrom, id).getSize()) > storageTo.getStorageSize()) {
-                throw new Exception();
-            }
-        } catch (Exception e) {
-            System.err.println("Storage " + storageTo.getId() + " size is not enough for transferring file " + id);
-            return;
-        }
+        validateStorageSizeForStorage(storageFrom, storageTo, id);
 
         for (int i = 0; i < storageTo.getFiles().length; i++) {
             try {
                 put(storageTo, getFileById(storageFrom, id));
                 return;
             } catch (Exception e) {
-                System.err.println("File with id " + id + " was not transferred to " + storageTo.getId());
+                System.err.println("File with id " + id + " was not transferred to " + storageTo.getId() + "Reason: " + e.getMessage() + " Reason: " + e.getMessage());
             }
         }
-
-    }
-
-
-    protected static long getStorageActualSize(Storage storage) {
-        long storageActualSize = 0;
-        for (File f : storage.getFiles()) {
-            if (f != null)
-                storageActualSize += f.getSize();
-        }
-        return storageActualSize;
-    }
-
-    protected static boolean filePresent(Storage storage, File file) throws Exception {
-        boolean filePresent = false;
-        for (File f : storage.getFiles()) {
-            if (f != null && f.getId() == file.getId())
-                filePresent = true;
-        }
-        return filePresent;
-    }
-
-
-    protected static File getFileById(Storage storage, long id) throws Exception {
-        File foundFile = null;
-        for (File f : storage.getFiles()) {
-            if (f != null && f.getId() == id)
-                foundFile = f;
-        }
-        return foundFile;
     }
 
 }
--- src/lesson19/hometask/Demo.java
+++ src/lesson19/hometask/Demo.java
@@ -24,10 +24,10 @@ public class Demo {
         File[] filesB = {file1, file3, file5, file6, file7};
 
 
-//        System.out.println(file);
-//        System.out.println(file1);
-//        System.out.println("_______________________________________");
-//
+        System.out.println(file);
+        System.out.println(file1);
+        System.out.println("_______________________________________");
+
         Storage storage = new Storage(0, filesX, formatsSupported ,"UK", 10);
         Storage storage1 = new Storage(1, filesX, formatsSupported ,"UK", 1000);
         Storage storage2 = new Storage(2, filesX, formatsSupported ,"UK", 900);
@@ -35,82 +35,121 @@ public class Demo {
         Storage storage4 = new Storage(4, filesX, formatsSupported ,"UK", 1000);
         Storage storage5 = new Storage(5, filesX, formatsSupported ,"UK", 1000);
         Controller controller = new Controller();
-//
-//
-//        for (File f : files) {
-//            try {
-//                controller.put(storage1, f);
-//            } catch (Exception e) {
-//                System.out.println("File with id " + f.getId() + "was not transferred!");
-//            }
-//        }
-//        System.out.println(Arrays.deepToString(storage1.getFiles()));
-//
-//        for (File f : files) {
-//            try {
-//                controller.put(storage2, f);
-//            } catch (Exception e) {
-//                System.out.println("File with id " + f.getId() + "was not transferred!");
-//            }
-//        }
-//        System.out.println(Arrays.deepToString(storage2.getFiles()));
-//        System.out.println("_______________________________________");
-//
-//
-//
-//
-//        controller.delete(storage1, file6);
-//        System.out.println(Arrays.deepToString(storage1.getFiles()));
-//
-//        controller.delete(storage2, file6);
-//        System.out.println(Arrays.deepToString(storage2.getFiles()));
-//        System.out.println("_______________________________________");
-//
-//
-//        try {
-//            controller.put(storage1, file7);
-//        } catch (Exception e) {
-//            System.out.println("File with id " + file7.getId() + "was not transferred!");
-//        }
-//        try {
-//            controller.put(storage2, file8);
-//        } catch (Exception e) {
-//            System.out.println("File with id " + file7.getId() + "was not transferred!");
-//        }
-//
-//        controller.transferAll(storage1, storage3);
-//        System.out.println("storage3" + Arrays.toString(storage3.getFiles()));
-//
-//        controller.transferAll(storage1, storage2);
-//        System.out.println("storage2" + Arrays.toString(storage2.getFiles()));
-//
-//
-//        System.out.println("TRANSFER FILE_______________________________________");
-//
-//        controller.transferFile(storage1, storage5, 3);
-//        System.out.println("storage5" + Arrays.toString(storage5.getFiles()));
-//
-//        controller.transferFile(storage2, storage5, 3);
-//        System.out.println("storage5" + Arrays.toString(storage5.getFiles()));
-//
-//        controller.transferFile(storage1, storage5, 1);
-//        System.out.println("storage5" + Arrays.toString(storage5.getFiles()));
-//
-//        controller.transferFile(storage1, storage5, 10);
-//        System.out.println("storage5" + Arrays.toString(storage5.getFiles()));
-//
-//
-//        controller.transferFile(storage1, storage5, 4);
-//        System.out.println("storage5" + Arrays.toString(storage5.getFiles()));
-//
-//        controller.transferFile(storage1, storage5, 5);
-//        System.out.println("storage5" + Arrays.toString(storage5.getFiles()));
-//
-//        controller.transferFile(storage1, storage5, 7);
-//        System.out.println("storage5" + Arrays.toString(storage5.getFiles()));
-//
-//        controller.transferFile(storage2, storage5, 8);
-//        System.out.println("storage5" + Arrays.toString(storage5.getFiles()));
+
+
+        for (File f : files) {
+            try {
+                controller.put(storage1, f);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(Arrays.deepToString(storage1.getFiles()));
+
+        for (File f : files)
+            try {
+                controller.put(storage2, f);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        System.out.println(Arrays.deepToString(storage2.getFiles()));
+        System.out.println("_______________________________________");
+
+
+
+
+        controller.delete(storage1, file6);
+        System.out.println(Arrays.deepToString(storage1.getFiles()));
+
+        controller.delete(storage2, file6);
+        System.out.println(Arrays.deepToString(storage2.getFiles()));
+        System.out.println("_______________________________________");
+
+
+        try {
+            controller.put(storage1, file7);
+        } catch (Exception e) {
+            System.out.println("File with id " + file7.getId() + "was not transferred!");
+        }
+        try {
+            controller.put(storage2, file8);
+        } catch (Exception e) {
+            System.out.println("File with id " + file7.getId() + "was not transferred!");
+        }
+
+        try {
+            controller.transferAll(storage1, storage3);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("storage3" + Arrays.toString(storage3.getFiles()));
+
+        try {
+            controller.transferAll(storage1, storage2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("storage2" + Arrays.toString(storage2.getFiles()));
+
+
+        System.out.println("TRANSFER FILE_______________________________________");
+
+        try {
+            controller.transferFile(storage1, storage5, 3);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("storage5" + Arrays.toString(storage5.getFiles()));
+
+        try {
+            controller.transferFile(storage2, storage5, 3);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("storage5" + Arrays.toString(storage5.getFiles()));
+
+        try {
+            controller.transferFile(storage1, storage5, 1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("storage5" + Arrays.toString(storage5.getFiles()));
+
+        try {
+            controller.transferFile(storage1, storage5, 10);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("storage5" + Arrays.toString(storage5.getFiles()));
+
+
+        try {
+            controller.transferFile(storage1, storage5, 4);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("storage5" + Arrays.toString(storage5.getFiles()));
+
+        try {
+            controller.transferFile(storage1, storage5, 5);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("storage5" + Arrays.toString(storage5.getFiles()));
+
+        try {
+            controller.transferFile(storage1, storage5, 7);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("storage5" + Arrays.toString(storage5.getFiles()));
+
+        try {
+            controller.transferFile(storage2, storage5, 8);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("storage5" + Arrays.toString(storage5.getFiles()));
 
 
         Storage storage6 = new Storage(6, filesA, formatsSupported, "UA", 3000);
@@ -119,13 +158,19 @@ public class Demo {
         Storage storage8 = null;
         Storage storage9 = null;
 
-        controller.transferAll(storage7, storage6);
+        try {
+            controller.transferAll(storage7, storage6);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println("storage6" + Arrays.toString(storage6.getFiles()));
 
-        controller.transferAll(storage6, storage7);
+        try {
+            controller.transferAll(storage6, storage7);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println("storage7" + Arrays.toString(storage7.getFiles()));
-
-
     }
 
 }
