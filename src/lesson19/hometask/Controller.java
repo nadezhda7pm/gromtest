d6d613126bb41857a225dd83c43b6308c5ad77c1
cdadmin <nadezhda7pm@gmail.com>
2019-02-21 17:07:25 +0200

    18th commit, unit 19 home task

--- src/lesson19/hometask/Controller.java
+++ src/lesson19/hometask/Controller.java
@@ -65,4 +65,100 @@ public class Controller {
         }
     }
 
+
+//    Validation___________________________________________________
+
+    private static boolean validationForPut(Storage storage, File file) throws Exception {
+        boolean validationForPut = false;
+        validateFileForNULL(file);
+        validateStorageForNULL(storage);
+        // if file format is wrong
+        validateFileFormat(storage, file);
+        // if storage size is not enough for the putting file
+        validateStorageSizeForFile(storage, file);
+        // if file exists in the storage
+        validateFilePresent (storage, file);
+        validationForPut = true;
+        return validationForPut;
+    }
+
+
+
+
+    private static void validateStorageSizeForFile(Storage storage, File file) throws Exception {
+        if (getStorageActualSize(storage) + file.getSize() > storage.getStorageSize())
+            throw new Exception("Storage " + storage.getId() + " size is not enough for the putting file " + file.getId());
+    }
+
+    private static void validateStorageFromToSize(Storage storageFrom, Storage storageTo) throws Exception {
+        if (getStorageActualSize(storageFrom) > storageTo.getStorageSize())
+            throw new Exception("Storage " + storageTo.getId() + " size is not enough for transfer");
+    }
+
+    private static void validateStorageSizeForStorage(Storage storageFrom, Storage storageTo, long id) throws Exception {
+        if ((getStorageActualSize(storageTo) + getFileById(storageFrom, id).getSize()) > storageTo.getStorageSize())
+            throw new Exception("Storage " + storageTo.getId() + " size is not enough for transferring file " + id);
+    }
+
+    private static void validateFileFormat(Storage storage, File file) throws Exception {
+        int c = 0;
+        for (String format : storage.getFormatsSupported()) {
+            if (file.getFormat().equals(format))
+                c++;
+        }
+        if (c < 1) throw new Exception("File " + file.getId() + " format is not acceptable");
+    }
+
+    private static void validateFilePresent(Storage storage, File file) throws Exception {
+        if (filePresent(storage, file))
+            throw new Exception("File with id " + file.getId() + " already exists in the storage");
+    }
+
+    private static void validateFileForNULL(File file) throws Exception {
+        if (file == null) throw new Exception("File is null");
+    }
+
+    private static void validateStorageForNULL(Storage storage) throws Exception {
+        if (storage == null) throw new Exception("Storage is null");
+    }
+
+    private static void validateStorageToFilesArrayLength(Storage storageFrom, Storage storageTo) throws Exception {
+        if (storageFrom.getFiles().length > storageTo.getFiles().length)
+            throw new ArrayIndexOutOfBoundsException("FilesArray length in storageTo is not enough for transfer");
+    }
+
+    private static void validateFilePresentInStorageFrom(Storage storageFrom, long id) throws Exception {
+        if (getFileById(storageFrom, id) == null)
+            throw new Exception("File with id " + id + " not found in storage " + storageFrom.getId() + " Reason: " );
+    }
+
+
+
+
+    private static File getFileById(Storage storage, long id) {
+        File foundFile = null;
+        for (File f : storage.getFiles()) {
+            if (f != null && f.getId() == id)
+                foundFile = f;
+        }
+        return foundFile;
+    }
+
+    private static long getStorageActualSize(Storage storage) {
+        long storageActualSize = 0;
+        for (File f : storage.getFiles()) {
+            if (f != null)
+                storageActualSize += f.getSize();
+        }
+        return storageActualSize;
+    }
+
+    private static boolean filePresent(Storage storage, File file) {
+        boolean filePresent = false;
+        for (File f : storage.getFiles()) {
+            if (f != null && f.getId() == file.getId())
+                filePresent = true;
+        }
+        return filePresent;
+    }
 }
