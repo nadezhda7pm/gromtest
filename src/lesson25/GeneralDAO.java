21d89b9c56db4660f877cd1358b34b5a79d7922c
cdadmin <nadezhda7pm@gmail.com>
2019-04-02 22:03:41 +0300

    52th commit, unit 25

---
+++ src/lesson25/GeneralDAO.java
@@ -0,0 +1,24 @@
+package lesson25;
+
+public class GeneralDAO<T> {
+
+    private void print(Order order){
+        System.out.println("order is:" + order.toString());
+
+    }
+
+
+    public <T extends IdEntity> void validate(T t) throws Exception{
+        if(t.getId() <= 0)
+            throw new Exception("id cannot be negative");
+    }
+
+    public <K> void validate(K k){
+        if (k.equals(100))
+            System.out.println("true");
+        else
+            System.out.println("false");
+    }
+
+
+}
