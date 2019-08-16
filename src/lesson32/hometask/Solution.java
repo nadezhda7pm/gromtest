5b4e36e83e565b28f3c252bdd41cce8923400df6
cdadmin <nadezhda7pm@gmail.com>
2019-08-16 17:19:01 +0300

    66th commit, unit 32

--- src/lesson32/hometask/Solution.java
+++ src/lesson32/hometask/Solution.java
@@ -8,26 +8,33 @@ import java.util.ArrayList;
 public class Solution {
 
 
-    public void readNumbers() throws IOException {
+    public Integer readNumbers() throws IOException {
 
         InputStreamReader reader = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(reader);
         Integer result = 0;
-        int n = 3;
-        while (n > 0) {
+        int n = 2;
+        while (n >= 0) {
+
             String inputNumbers = br.readLine();
 
             if (validateInput(inputNumbers) != null) {
                 for (Integer i : validateInput(inputNumbers)) {
        //             if (i != null)
                         result += i;
-                    System.out.println(result);
                 }
+                    System.out.println(result);
+                return result;
             }
             else{
+
+                if (n == 0){
+                    System.out.println("“Your numbers are wrong. Number of attempts exceeded”");
+                }
+                else
                     System.out.println("“Your numbers are wrong. You have " + n + " attempts to try again");
                 }n--;
-            }
+            }return result;
         }
 
 
@@ -38,7 +45,7 @@ public class Solution {
         String[] strs = input.split(" ");
         ArrayList<Integer> numbers = new ArrayList<>();
 
-        if (strs.length >= 10) {
+        if (strs.length > 10) {
             System.out.println("more than 10");
             return null;
         }
